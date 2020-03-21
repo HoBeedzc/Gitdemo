cd3edd043fe17f8f37061d73ee4baf769b0dd23a
Hobee <lzqpublic@163.com>
2020-03-21 23:28:40 +0800

    second commit

---
+++ GitHubItems.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# ©copyright 1999-2020 Hobee. All rights reserved.
+# www.hobeedzc.com
+# mailto:LZQpublic@163.com
+# Never Settle
+
+import random
+import math
+import numpy as np
